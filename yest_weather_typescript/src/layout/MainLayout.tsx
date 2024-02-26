2c6fcd4b8550a58506c49fbf58550dcd2c33b766
kimjeju <h.hwi9367@gmail.com>
2024-02-26 22:20:19 +0900

    test

--- yest_weather_typescript/src/layout/MainLayout.tsx
+++ yest_weather_typescript/src/layout/MainLayout.tsx
@@ -14,11 +14,8 @@ const MainForm = styled.div`
 
     //스타일
     width : 400px;
-    height : 800px;
     background-color : red;
-    border : 3px solid black;
     box-shadow: 5px 5px lightgrey;
-    border-radius : 30px;
     transition : 0.5s;
 `
 
